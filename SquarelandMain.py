9a5fe47d115402e1ce71911f04696c84624f827c
Jefferson Quesado <jeff.quesado@gmail.com>
2016-09-01 19:56:29 -0300

    Usando um level =3

--- SquarelandMain.py
+++ SquarelandMain.py
@@ -1,4 +1,6 @@
 if __name__ == '__main__':
+    from elements.level import Level
     from game import SquareLandGame
     game = SquareLandGame()
+    game.create_level(Level())
     game.run_game()
