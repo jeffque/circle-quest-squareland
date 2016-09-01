f3eaadfc2b43d3282beaecaf4c518d2a41ba2847
Jefferson Quesado <jeff.quesado@gmail.com>
2016-09-01 14:29:48 -0300

    Pondo um retângulo para cair com a gravidade

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import os, sys
 import pygame
 from pygame.locals import *
+from elements.physics import Retangulo
 
 if not pygame.font: print('Warning, fonts disabled')
 if not pygame.mixer: print('Warning, sound disabled')
@@ -13,10 +14,25 @@ class SquareLandGame:
         self.height = SquareLandGame.height
         pygame.init()
         self.screen = pygame.display.set_mode((self.width, self.height))
+        self.ret = Retangulo()
+        self.ret.falling = True
 
 
     def run_game(self):
+        tique_taque = pygame.time.Clock()
+        acc = 0
         while True:
+            self.screen.fill((0,0,0))
+            delta_seconds = (tique_taque.get_time()) / 1000
+            acc += delta_seconds
+            tique_taque.tick(100)
+
+            self.ret.cair(delta_seconds)
+            self.ret.movimento(delta_seconds)
+            print('intervalo em segunds %f' % delta_seconds)
+            print('tempo desde o começo em segunds %f' % acc)
+            self.ret.render(self.screen)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            pygame.display.update()
