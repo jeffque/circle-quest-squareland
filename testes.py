1181e43db24a4bf687ca1658d96173c0b2cbc053
Jefferson Quesado <jeff.quesado@gmail.com>
2016-09-01 14:30:13 -0300

    aumentando os testes um conto

--- testes.py
+++ testes.py
@@ -1,4 +1,4 @@
-from elements.geometry import direction2module
+from elements.geometry import direction2module, coords_soma
 
 print(direction2module([1, 1], 1))
 print(direction2module([1, 0], 1))
@@ -9,3 +9,12 @@ print(direction2module([1, 1], 9))
 print(direction2module([1, 0], 9))
 print(direction2module([0, 1], 9))
 print(direction2module([0, 0], 9))
+
+velocidade = direction2module([1, 0], 0.026)
+print(velocidade)
+pos = [0,0]
+print(pos)
+pos = coords_soma(pos, velocidade)
+print(pos)
+pos = coords_soma(pos, velocidade)
+print(pos)
